b1d41499e54fd9c715faf59943bf8a5399b39c09
Dan Caddigan <goldcaddy77@gmail.com>
2020-10-02 22:31:29 -0400

    fix(deploy): fix deploy issues again

--- tools/scripts/start.sh
+++ tools/scripts/start.sh
@@ -1,12 +1,23 @@
-# import .env file
-set -a
-. ./.env >/dev/null 2>&1
-set +a
+# Add local environment variables first
+if [ -f .env.local ]; then
+  echo ".env.local exists."
 
+  set -a
+  . ./.env.local >/dev/null 2>&1
+  set +a
+fi
+
+# Then run config to generate our .env
 if [ -z "$WARTHOG_ENV" ]; then
   WARTHOG_ENV=local
 fi
 yarn run config
+
+
+# # Now import .env into the shell for 
+# set -a
+# . ./.env >/dev/null 2>&1
+# set +a
 
 if [ -z "$WARTHOG_BUILD_ENV" ]; then
   yarn ts-node --type-check src/index.ts
--- tools/scripts/test.sh
+++ tools/scripts/test.sh
@@ -1,9 +1,10 @@
+WARTHOG_ENV=test yarn run config
+
 # import .env file
 set -a
 . ./.env >/dev/null 2>&1
 set +a
 
-WARTHOG_ENV=test yarn run config
 sleep 1 # at times, the new config is not picked up and Prod config will creep in
 yarn db:drop
 yarn db:create
