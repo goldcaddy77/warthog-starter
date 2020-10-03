97c9c14d7af0f3bccbedee837036b28eebbc72c4
Dan Caddigan <goldcaddy77@gmail.com>
2020-10-02 21:38:16 -0400

    fix(deploy): fix deploy issues again

--- tools/scripts/test.sh
+++ tools/scripts/test.sh
@@ -4,7 +4,7 @@ set -a
 set +a
 
 WARTHOG_ENV=test yarn run config
-sleep 1
+sleep 1 # at times, the new config is not picked up and Prod config will creep in
 yarn db:drop
 yarn db:create
 yarn jest --verbose
