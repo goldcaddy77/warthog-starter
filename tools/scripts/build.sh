37c80a9f5c8e924a79cc69ca704e06e99d3c5145
Dan Caddigan <goldcaddy77@gmail.com>
2020-10-03 01:26:19 -0400

    fix(deploy): fix deploy issues again (last one)

--- tools/scripts/build.sh
+++ tools/scripts/build.sh
@@ -1,7 +1,19 @@
-# This is the script that is run as part of the build process, so it needs to do
+# This is the script that is run as part of the Heroku build process, so it needs to do
 # everything to get a Production system built
+# If STACK is set, it means we're in the Heroku build ENV
 
-env
-NODE_ENV=production yarn run config
+# Add local environment variables first
+if [ -f .env.local ]; then
+  set -a
+  . ./.env.local >/dev/null 2>&1
+  set +a
+fi
+
 yarn run codegen
 yarn run compile
+
+### HEROKU ###
+# Rebuild .env after codegen (which sets it to development values)
+if test -n "${STACK-}"; then
+  yarn run config
+fi
